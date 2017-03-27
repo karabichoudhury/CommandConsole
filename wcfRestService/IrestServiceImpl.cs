05880a358dd759c2e77de0b397a4514eea8b0b36
karabichoudhury <mailto_karabi@yahoo.com>
2017-03-27 23:32:16 +0530

    Adding WCF Rest service

---
+++ wcfRestService/IrestServiceImpl.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace wcfRestService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IrestServiceImpl" in both code and config file together.
+    [ServiceContract]
+    public interface IrestServiceImpl
+    {
+    
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "json/{id}")]
+        data readData(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "write")]
+        string writeData(data rData);
+    }
+
+    [DataContract]
+    public class Data
+    {
+        [DataMember]
+        public string details { get; set; }
+    }
+
+    [DataContract]
+    public class data
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Age { get; set; }
+
+        [DataMember]
+        public string Exp { get; set; }
+
+        [DataMember]
+        public string Technology { get; set; }
+
+    }
+}
---
+++ wcfRestService/restServiceImpl.svc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcfRestService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "restServiceImpl" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select restServiceImpl.svc or restServiceImpl.svc.cs at the Solution Explorer and start debugging.
+    public class restServiceImpl : IrestServiceImpl
+    {
+        
+        public data readData(string id)
+        {
+            var obj = new data
+            {
+                Name = "Ram",
+                Age = "25",
+                Exp = "8",
+                Technology = "Dotnet"
+            };
+            return obj;
+        }
+
+
+        public string writeData(data rData)
+        {
+            
+            return "record inserted";
+        }
+    }
+}
