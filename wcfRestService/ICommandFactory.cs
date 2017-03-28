e696facb36f98a9751a6a794c937d93d0e1ba6eb
karabichoudhury <mailto_karabi@yahoo.com>
2017-03-28 17:33:52 +0530

    adding command pattern

---
+++ wcfRestService/ICommandFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wcfRestService
+{
+    public interface ICommandFactory
+    {
+        string CommandName { get; }
+        string Description { get; }
+
+        ICommand MakeCommand(string[] arguements);
+    }
+}
---
+++ wcfRestService/updateOrderCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace wcfRestService
+{
+    public class updateCommand : ICommand, ICommandFactory
+    {
+        public string CommandName
+        {
+            get { return "Update"; }
+        }
+
+        public string Description
+        {
+            get { return "Update the Db"; }
+        }
+
+        public int NewQuantity { get; set; }
+        public ICommand MakeCommand(string[] arguements)
+        {
+            return new updateCommand { NewQuantity = int.Parse(arguements[1]) };
+        }
+
+        public void Execute()
+        {
+            const int oldQuantity = 5;
+            Console.WriteLine("DATABASE:Updated");
+
+            Console.WriteLine("LOG:Updated order quantity from {0} to {1}", oldQuantity, NewQuantity);
+            
+        }
+    }
+}
